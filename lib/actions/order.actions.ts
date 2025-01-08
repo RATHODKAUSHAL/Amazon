b37878344cba096ed00c8b23eae52a8647809ea3
RATHOD KAUSHAL <rathodkaushal2001@gmail.com>
2025-01-08 10:55:14 +0530

    create-order-history-page

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -3,7 +3,7 @@ import { Cart, OrderItem, ShippingAddress } from '@/types'
 import { formatError, round2 } from '../utils'
 import { connectToDatabase } from '../db'
 import { auth } from '@/auth'
-import { AVAILABLE_DELIVERY_DATES } from '../constants'
+import { AVAILABLE_DELIVERY_DATES, PAGE_SIZE  } from '../constants'
 import { OrderInputSchema } from '../validator'
 import Order, { IOrder } from '../db/models/order.model'
 import { paypal } from '../paypal'
@@ -170,5 +170,33 @@ const taxPrice = !shippingAddress ? undefined : round2(itemsPrice * 0.15)
     shippingPrice,
     totalPrice,
     taxPrice,
+  }
+}
+
+export async function getMyOrders({
+  limit,
+  page,
+}: {
+  limit?: number
+  page: number
+}) {
+  limit = limit || PAGE_SIZE
+  await connectToDatabase()
+  const session = await auth()
+  if (!session) {
+    throw new Error('User is not authenticated')
+  }
+  const skipAmount = (Number(page) - 1) * limit
+  const orders = await Order.find({
+    user: session?.user?.id,
+  })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(limit)
+  const ordersCount = await Order.countDocuments({ user: session?.user?.id })
+
+  return {
+    data: JSON.parse(JSON.stringify(orders)),
+    totalPages: Math.ceil(ordersCount / limit),
   }
 }
